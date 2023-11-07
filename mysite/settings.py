f86762910d89942b65b516c2ac68c450c9163803
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-11-07 23:13:03 +0530

    updated requirements

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,6 +79,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'attendance', 
+    #     'USER': 'attendance_user',
+    #     'PASSWORD': 'attendance_password',
+    #     'HOST': '127.0.0.1', 
+    #     'PORT': '5432',
+    # }
 }
 
 

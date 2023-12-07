3d53f23ff4a88b0cc3866cbdbef70deb4c0a71a7
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-12-08 01:56:16 +0530

    updated settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,22 +100,6 @@ else:
         "NAME": BASE_DIR / "db.sqlite3",
     }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-#     # 'default': dj_database_url.parse('postgres://attendance_tiat_user:8P7NXHCMqAUPdUjAquxVmNaCFEE8y8Do@dpg-cl578dh82rpc73fqbeog-a.singapore-postgres.render.com/attendance_tiat', conn_max_age=600),
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#     #     'NAME': 'attendance',
-#     #     # 'USER': 'attendance_tiat_user',
-#     #     # 'PASSWORD': '8P7NXHCMqAUPdUjAquxVmNaCFEE8y8Do',
-#     #     'url': 'postgres://attendance_tiat_user:8P7NXHCMqAUPdUjAquxVmNaCFEE8y8Do@dpg-cl578dh82rpc73fqbeog-a.singapore-postgres.render.com/attendance_tiat',
-#     # }
-# }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
